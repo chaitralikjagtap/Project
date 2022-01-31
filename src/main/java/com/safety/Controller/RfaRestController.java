540156d84cbb373b2c73cbb8517dae978900b8d3
Jagtap <chaitrali.jagtap@capgemini.com>
2022-01-31 18:38:23 +0530

    First commit

--- src/main/java/com/safety/Controller/RfaRestController.java
+++ src/main/java/com/safety/Controller/RfaRestController.java
@@ -12,12 +12,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.safety.Exception.RecordNotFoundException;
 import com.safety.Model.Rfa;
 import com.safety.Service.RfaService;
 
+@RequestMapping("/rfa")
 @RestController
 public class RfaRestController {
 
